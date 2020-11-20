9aeff47d0b31bfc84d424f93d8066a88df9f228c
bwcs99 <45511098+bwcs99@users.noreply.github.com>
2020-11-20 17:14:36 +0100

    Add files via upload
    Oto wersja alpha z zintegrowanym serwerem. Gracz otwiera okna powitalne, przyłącza się do gry (nick, cywilizacja), dostaje od serwera wygenerowaną mape i informacje o innych graczach w grze (potrzebne do okna lobby). Generowanie mapy rozdzieliło się na generowanie macierzy po stronie serwera i przerabianie wysłanej macierzy na obraz. Są 3 problemy:
    - trzeba zintegorwać obraz mapy z mapą gry (kod Patryka)
    - trzeba generować małe mapy (300x300,200x200,100x100), bo tworzenie dużych spowalnia działanie serwera i są problemy z poprawnym wysyłaniem tak dużych macierzy. W związku z tym, trzeba będzie powiększać otrzymane obrazy (bardziej uwaga niż problem).
    - Nie wyświetlały mi się zdjęcia w oknach startowych (nie wiem czemu. Przeniosłem wszystkie obrazy do katalogu start_screens, ale to też nie pomogło)

---
+++ alpha/game_screens/game.py
@@ -0,0 +1,25 @@
+import arcade
+from game_view import GameView
+
+SCREEN_WIDTH = 1080
+SCREEN_HEIGHT = 720
+
+
+class Game(arcade.Window):
+    def __init__(self, width, height, tiles):
+        super().__init__(width, height, "Age of Divisiveness")
+        self.game_view = GameView(width, height, tiles)
+        self.sprites = arcade.SpriteList()
+        self.zoom = 0
+        self.back_to_game()
+
+    def back_to_game(self):
+        self.show_view(self.game_view)
+
+    def setup(self):
+        """ Set up the game here. Call this function to restart the game. """
+        pass
+
+    def run(self):
+        arcade.run()
+
---
+++ alpha/main.py
@@ -0,0 +1,22 @@
+import sys
+from random import randrange
+
+from PyQt5.QtWidgets import QApplication
+
+from game_screens import Game
+from start_screens.welcome_window import WelcomeWindow
+
+SCREEN_WIDTH = 1080
+SCREEN_HEIGHT = 720
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    win = WelcomeWindow()
+    win.show()
+    app.exec_()
+    # print(win.map_generator_window.chosen_nick)
+    # uruchom ekrany startowe i niech one zrobia swoje w kwestiach serwera
+    # niech zwracaja macierz mapy
+    world_map = [[randrange(0, 4) for _ in range(40)] for _ in range(25)]
+    window = Game(SCREEN_WIDTH, SCREEN_HEIGHT, world_map)
+    window.run()
