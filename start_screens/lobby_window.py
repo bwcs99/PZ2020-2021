7dd1ed6002e5d4307225d17dc16b04be68863045
GabrielWechta <wechtagabriel@gmail.com>
2020-11-25 00:17:37 +0100

    Works better than ever

--- start_screens/lobby_window.py
+++ start_screens/lobby_window.py
@@ -1,5 +1,6 @@
 import sys
 import ast
+import threading
 from PIL.ImageQt import ImageQt
 from PyQt5.QtCore import QRect
 from PyQt5.QtGui import QPixmap
@@ -20,9 +21,10 @@ class LobbyWindow(QMainWindow):
         """If player is hosting game, constructor should receive True as parameter, else false"""
         super(LobbyWindow, self).__init__()
         self.players_table = None
-        self.map = QLabel(self)
+        self.map_label = QLabel(self)
         self.launch_button = None
         self.init_ui(are_you_host)
+        self.game_map = None
 
         if are_you_host is True:
             self.client = Client()
@@ -38,12 +40,15 @@ class LobbyWindow(QMainWindow):
             nick, civ, col = player_string.split(":")
             self.add_player_to_table([nick, civ, col])
 
-        tmp_map = self.client.get_map_from_server()
-        tmp_map = ast.literal_eval(tmp_map)
-        image = img_gen.get_map_overview(tmp_map)
+        self.game_map = self.client.get_map_from_server()
+        self.game_map = ast.literal_eval(self.game_map)
+        image = img_gen.get_map_overview(self.game_map)
         qim = ImageQt(image).copy()
         new_map = QPixmap.fromImage(qim)
-        self.map.setPixmap(new_map)
+        self.map_label.setPixmap(new_map)
+
+        waiting_for_new_players = threading.Thread(target=self.wait_for_new_players, args=(are_you_host, ))
+        waiting_for_new_players.start()
 
     def init_ui(self, are_you_host: bool):
         self.setWindowTitle("Age of Divisiveness - Game lobby")
@@ -56,8 +61,8 @@ class LobbyWindow(QMainWindow):
         self.players_table.setHorizontalHeaderLabels(["Nickname", "Civilization", "Color"])
         header.setSectionResizeMode(0, QHeaderView.Stretch)
 
-        self.map = QLabel(self)
-        self.map.setGeometry(QRect(480, 30, 570, 570))
+        self.map_label = QLabel(self)
+        self.map_label.setGeometry(QRect(480, 30, 570, 570))
 
         # TODO this path should determine place where map sent by server_utils is.
 
@@ -68,8 +73,8 @@ class LobbyWindow(QMainWindow):
         # pixmap = QPixmap(qim)
 
         pixmap = QPixmap('/resources/images/example_map_2.png')  # and delete this after uncommenting above
-        self.map.setPixmap(pixmap)
-        self.map.setScaledContents(True)
+        self.map_label.setPixmap(pixmap)
+        self.map_label.setScaledContents(True)
 
         self.launch_button = QPushButton(self)
         self.launch_button.setText("Launch")
@@ -91,15 +96,25 @@ class LobbyWindow(QMainWindow):
             self.players_table.setItem(row_position, i, QTableWidgetItem(peace))
 
     def __launch_game(self):
-        # TODO rozpoczęcie rozgrywki czyli zamknięcie tego okna
-        print("game is starting")
-        self.close()
+        self.client.exit_lobby()
 
     def center(self):
         qr = self.frameGeometry()
         cp = QDesktopWidget().availableGeometry().center()
         qr.moveCenter(cp)
         self.move(qr.topLeft())
+
+    def wait_for_new_players(self, are_you_host):
+        while True:
+            new_player_info = self.client.get_new_player()
+            if new_player_info[0] == "FINISH":
+                break
+            else:
+                self.add_player_to_table(new_player_info[1:])
+        if are_you_host is True:
+            self.client.start_game()
+        self.close()
+
 
 
 # for testing
