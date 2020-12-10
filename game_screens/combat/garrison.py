c415d805bca8c5a1b7eb9a288c1e0c6d1d7b6aed
krzysztof-szymaniak <krzysztof.szymaniak1004@gmail.com>
2020-12-10 19:43:50 +0100

    added soldier property: max_movement

--- game_screens/combat/garrison.py
+++ game_screens/combat/garrison.py
@@ -3,11 +3,11 @@ from game_screens.units import Unit
 # a concept for test purposes, feel free to change or add more
 """ Dictionary of soldiers' properties. """
 SOLDIER_PROPS = [
-    {'type': 'ARCHER', 'health': 5, 'damage': 20, 'probability': 0.4},
-    {'type': 'KNIGHT', 'health': 15, 'damage': 5, 'probability': 0.8},
-    {'type': 'CAVALRY', 'health': 10, 'damage': 10, 'probability': 0.6},
-    {'type': 'DUCK', 'health': 1, 'damage': 1, 'probability': 0.15},
-    {'type': 'PANZERKAMPFWAGEN VI TIGER', 'health': 500, 'damage': 500, 'probability': 0.5},
+    {'type': 'ARCHER', 'health': 5, 'damage': 20, 'probability': 0.4, 'max_movement': 5},
+    {'type': 'KNIGHT', 'health': 15, 'damage': 5, 'probability': 0.8, 'max_movement': 2},
+    {'type': 'CAVALRY', 'health': 10, 'damage': 10, 'probability': 0.6, 'max_movement': 15},
+    {'type': 'DUCK', 'health': 1, 'damage': 1, 'probability': 0.15, 'max_movement': 1},
+    {'type': 'PANZERKAMPFWAGEN VI TIGER', 'health': 500, 'damage': 500, 'probability': 0.5, 'max_movement': 50}
 ]
 
 
@@ -20,4 +20,5 @@ class Garrison(Unit):
         self.health = soldier_stats['health'] * count
         self.probability = soldier_stats['probability']
         self.count = count
-        # super().__init__(tile, owner)  # TODO uncomment when done testing
+        self.max_movement = self.movement = soldier_stats['max_movement']
+        super().__init__(tile, owner)  # TODO uncomment when done testing
